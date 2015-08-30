1abcb53b6dfa33790f6c691c537e40783bb36453
Kebing Yu <kyu-imac@Kebings-iMac.local>
2015-08-29 18:05:27 -0700

    add cors support

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,17 @@
+<?php 
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Routing\Middleware;
+use Illuminate\Http\Response;
+
+class CORS implements Middleware
+{
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin' , '*')
+            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
+    }
+}
